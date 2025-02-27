84edd8d6a0248e3ddf5f80302574b2370bb47b72
abyss-s <lh3729@naver.com>
2025-02-27 23:52:02 +0900

    app router test

---
+++ my-app/src/app/test/page.tsx
@@ -0,0 +1,9 @@
+// pages/test/page.tsx: /test로 라우팅 연결
+export default function Test() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>This is a simple Next.js page.</p>
+    </div>
+  );
+}
