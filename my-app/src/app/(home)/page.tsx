9d3da7368dbedd833228ef776bef6f07a87ee4a3
abyss-s <lh3729@naver.com>
2025-03-03 00:48:27 +0900

    fetch and loading components

--- my-app/src/app/(home)/page.tsx
+++ my-app/src/app/(home)/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
-  return (
-    <div>
-      <main className="flex flex-col ">hello!</main>
-    </div>
-  );
+export const metadata = {
+  title: 'Home',
+};
+
+const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+
+async function getMovies() {
+  // await new Promise((res) => setTimeout(res, 3000));
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data;
+}
+
+export default async function Home() {
+  const movies = await getMovies();
+
+  return <div>{JSON.stringify(movies)}</div>;
 }
