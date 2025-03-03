79f8f9f9febee88c96a69edfbbab2eec5889c01a
abyss-s <lh3729@naver.com>
2025-03-03 18:10:09 +0900

    add: image domain config

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
       dynamic: 30, // 캐싱 추가
     },
   },
+  images: {
+    domains: ['image.tmdb.org'], // 외부 이미지를 불러오기 위한 도메인 추가
+  },
 };
 
 export default nextConfig;
