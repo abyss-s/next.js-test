9d9186a443dc1b16199258c75869351d1766d9c2
abyss-s <lh3729@naver.com>
2025-03-03 18:10:34 +0900

    design: css module test

--- my-app/src/app/(home)/page.tsx
+++ my-app/src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link';
+import Movie from '../../components/movie';
+import styles from '../styles/home.module.css';
 import { Key } from 'react';
 
 export const metadata = {
@@ -18,11 +19,12 @@ export default async function Home() {
   const movies = await getMovies();
 
   return (
-    <div>
-      {movies.map((movie: { title: string; id: Key }) => (
-        <li key={movie.id}>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+    <div className={styles.container}>
+      {movies.map((movie: { id: Key; title: string; poster_path: string }) => (
+        // <div key={movie.id}>
+        //   <Image src={movie.poster_path} alt={movie.title} width={200} height={300}></Image>
+        //   <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        <Movie key={movie.id} id={movie.id} poster_path={movie.poster_path} title={movie.title} />
       ))}
     </div>
   );
---
+++ my-app/src/components/movie.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '../app/styles/movie.module.css';
+import { useRouter } from 'next/navigation';
+
+interface IMovieProps {
+  title: string;
+  id: string;
+  poster_path: string;
+}
+
+export default function Movie({ title, id, poster_path }: IMovieProps) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push(`/movies/${id}`);
+  };
+
+  return (
+    <div className={styles.movie}>
+      <Image src={poster_path} alt={title} onClick={onClick} width={200} height={300} />
+      <Link href={`/movies/${id}`}>{title}</Link>
+    </div>
+  );
+}
