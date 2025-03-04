816ffb592a6d414deb72bd17306516a32129b85c
abyss-s <lh3729@naver.com>
2025-03-05 00:38:41 +0900

    add comments: for ts, react-query study

--- my-app/src/app/(home)/page.tsx
+++ my-app/src/app/(home)/page.tsx
@@ -4,23 +4,35 @@ import { useQuery } from '@tanstack/react-query';
 import Movie from '@/components/movie';
 import { fetchMovies } from '@/app/apis/movies';
 
+// 영화 객체 인터페이스 정의
+interface MovieType {
+  id: string;
+  poster_path: string;
+  title: string;
+}
+
 export default function Home() {
+  // useQuery 훅을 사용하여 영화 데이터를 가져옴
   const {
-    data: movies,
-    isError,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ['movies'],
-    queryFn: fetchMovies,
+    data: movies, // API의 응답 데이터
+    isError, // 에러 여부
+    isLoading, // 로딩 여부
+    error, // 에러 객체
+  } = useQuery<MovieType[]>({
+    queryKey: ['movies'], // 쿼리의 고유 키,
+    queryFn: fetchMovies, // 데이터를 가져오는 함수
+    initialData: [],
   });
 
+  // 로딩 중일 때 표시할 UI
   if (isLoading) return <div>Loading...</div>;
+  // 에러 발생 시 표시할 UI
   if (isError) return <div>Error loading movies: {error.message}</div>;
 
   return (
     <div className="container home">
-      {movies.map((movie) => (
+      {/* 각 영화를 키로 하여 대해 Movie 컴포넌트를 렌더링 */}
+      {movies?.map((movie) => (
         <Movie key={movie.id} id={movie.id} poster_path={movie.poster_path} title={movie.title} />
       ))}
     </div>
--- my-app/src/app/(movies)/movies/[id]/page.tsx
+++ my-app/src/app/(movies)/movies/[id]/page.tsx
@@ -1,26 +1,32 @@
 import { Suspense } from 'react';
 import MovieInfo, { getMovie } from '@/components/movie-info';
 import MovieVideos from '@/components/movie-video';
-import { Params } from 'next/dist/server/request/params';
 
-interface IParams {
-  params: Params;
+// Params 인터페이스 정의
+interface Params {
+  id: string;
 }
 
-// 메타데이터 생성 함수
-export async function generateMetadata({ params }: { params: { id: string } }) {
-  const { id } = params; // params에서 id를 직접 추출
+// MovieDetailProps 인터페이스 정의: Promise로 감싸 파라미터를 비동기적으로 처리
+interface MovieDetailProps {
+  params: Promise<Params>;
+}
+
+// 메타데이터 동적 생성 함수
+export async function generateMetadata({ params }: MovieDetailProps) {
+  const { id } = await params; // Promise에서 id 추출
   const movie = await getMovie(id);
   return {
-    title: movie.title, // 동적인 URL Params를 이용한 메타데이터 생성
+    title: movie.title, // 영화 제목으로 동적 메타데이터 title 설정
   };
 }
 
-export default async function MovieDetail({ params }: IParams) {
-  const { id } = params;
-
+// 비동기적으로 영화 정보를 가져와 화면에 렌더링
+export default async function MovieDetail({ params }: MovieDetailProps) {
+  const { id } = await params;
   return (
     <div>
+      {/* Suspense를 사용하여 각 컴포넌트를 비동기적으로 가져와 렌더링 */}
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
